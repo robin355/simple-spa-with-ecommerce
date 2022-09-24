5e3008fed450649029272c0069ccd4244a3023c8
robin mahmud <robinmahmud358@gmail.com>
2022-09-24 22:49:23 +0600

    fetch data

---
+++ src/components/Product/Product.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Product.css'
+const Product = (props) => {
+    const { name, img, price, seller, ratings
+    } = props.product
+    return (
+        <div className='product'>
+            <img src={img} alt="" srcset="" />
+            <p className='product-name'>{name}</p>
+
+        </div>
+    );
+};
+
+export default Product;
