e75ec60e51ad008539d313e9525c2b1e3d31545a
robin mahmud <robinmahmud358@gmail.com>
2022-09-26 08:03:55 +0600

    localStorage data relaod

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useEffect } from 'react';
 import { useState } from 'react';
+import { addToDb, getStoreCart } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css'
@@ -13,9 +14,35 @@ const Shop = () => {
             .then(data => setProducts(data))
 
     }, [])
-    const handleCartAdd = (product) => {
-        const newCart = [...cart, product]
+    useEffect(() => {
+        const storeCart = getStoreCart();
+        const saveCart = [];
+        for (const id in storeCart) {
+            const addedProduct = products.find(product => product.id === id)
+            if (addedProduct) {
+                const quantity = storeCart[id];
+                addedProduct.quantity = quantity
+                saveCart.push(addedProduct)
+            }
+        }
+        setCart(saveCart);
+
+    }, [products])
+
+    const handleCartAdd = (selectedproduct) => {
+        let newCart = [];
+        const exited = cart.find(product => product.id === selectedproduct.id)
+        if (!exited) {
+            selectedproduct.quantity = 1;
+            newCart = [...cart, selectedproduct];
+        }
+        else {
+            const rest = cart.filter(product => product.id !== selectedproduct.id);
+            exited.quantity = exited.quantity + 1;
+            newCart = [...rest, exited]
+        }
         setCart(newCart)
+        addToDb(selectedproduct.id)
     }
 
     return (
